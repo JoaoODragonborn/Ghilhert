0fd282356ff37c5d304ebef545af42705f38ccb6
JoaoODragonborn <jp1792464@gmail.com>
2025-03-18 00:39:48 -0300

    fix: inutilia truncat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import Table from './components/table/index.js';
-import MyApp from './components/test/index.js';
-
 
 function App() {
   return (
     <div className="App">
       <Table/>
-      <MyApp/>
     </div>
   );
 }
